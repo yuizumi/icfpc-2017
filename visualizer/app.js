e7fd985448420368b2e61e8b7d7959ac80224039
kaming <kaming@SAKURA-2.local>
2017-08-05 11:47:51 +0900

    Add button and change libs->cdn at index.html,add updateGame() and functions for button at app.js

--- visualizer/app.js
+++ visualizer/app.js
@@ -1,11 +1,40 @@
-function init_map(){
+function updateGame(Game){
+  console.log(Game);
+  let url = 'http://localhost/test/log/'+String(Game.gamenumber)+'/'+String(Game.turn);
   let dom = $('#main-visualize-cell');
-  $.ajax(
+
+  $.ajax({
     type:"GET",
     dataType:"json",
-    url:""
-  ).done(function(response, status, data){
-    Visualizer.visualize(dom,data);
+    url:url}
+  ).done(function(response){
+    if(Game.turn === 0){
+      console.log(Game.turn);
+      //Visualizer.visualize(dom,data);
+    }else{
+      console.log(Game.turn);
+      //Visualizer.update(dom,data)
+    }
+    console.log(response);
+    Game.turn++;
   });
-  )
 }
+
+var game = {}
+
+$('#play').on('click',function(){
+  game = {gamenumber:Number($('#gamenumber').val()),turn:0}
+  updateGame(game);
+});
+
+$('#next').on('click',function(){
+  updateGame(game);
+});
+
+$('#autoplay').on('click',function(){
+  updateGame(game);
+});
+
+$('#stopplay').on('click',function(){
+  updateGame(game);
+});
