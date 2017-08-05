dc871e6614b7343fcc81842c382e8b51d7c30fd0
cocodrips <cocodrips@gmail.com>
2017-08-05 15:59:50 +0900

    Add debug log.

--- visualizer/view/visualize.js
+++ visualizer/view/visualize.js
@@ -4,14 +4,18 @@
 
 // data = {};
 // data.nodes = [node_id, ...]
-// data.edge = [[src, dst], ...]
+// data.edges = [[src_id, dst_id], ...]
+// data.lambdas = [node_idx ...]
 
 var Visualizer = {};
 
 Visualizer.createMap = function (root_selector, data) {
+    console.log('selector', root_selector);
+    console.log('map_data:', data);
     return 0;
 };
 
-Visualizer.update = function (src_node_idx, dst_node_idx) {
+Visualizer.update = function (src_node_id, dst_node_id) {
+    console.log(src_node_id + '->' + dst_node_id);
     return 0;
 };
