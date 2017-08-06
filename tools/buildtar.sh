88b066ee7178dfbba4e2d994a1d11290aac632ec
Yusuke Izumi <yuizumi@y5i.org>
2017-08-06 15:58:52 +0900

    Updated buildtar.sh. (#33)

--- tools/buildtar.sh
+++ tools/buildtar.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 declare -r ID=d3aeebce-2ac4-49f9-9568-0b177a60c502
+declare -r ROOT_DIR="$(dirname $0)/.."
 
 if [[ -z "$1" ]]; then
     echo "${0##*/}: Must specify the AI binary." 2>&1
@@ -9,13 +10,13 @@ fi
 
 set -ex
 
-cd "$(dirname $0)/.."
-WORK_DIR="$(mktemp -d $HOME/build.XXXXXX)"
+WORK_DIR="$(mktemp -d ${HOME}/build.XXXXXX)"
+cd "${ROOT_DIR}"
 cp README.md "${WORK_DIR}/README"
 mkdir "${WORK_DIR}/src"
 cp -R [A-Za-z]* "${WORK_DIR}/src"
-(cd ai && cmake . && make "$1")
-cp -p ai/"$1" "${WORK_DIR}/punter"
+(cd build && cmake .. && make "$1")
+cp -p "build/ai/$1" "${WORK_DIR}/punter"
 cp -p tools/install.sh "${WORK_DIR}/install"
 touch "${WORK_DIR}/PACKAGES"
 tar cfz "${HOME}/icfp-${ID}.tar.gz" -C "${WORK_DIR}" .
