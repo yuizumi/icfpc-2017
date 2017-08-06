3d3b18f2690eda30598753b3b1a7027733c0cc72
cocodrips <cocodrips@gmail.com>
2017-08-06 22:10:41 +0900

    Test.

---
+++ ai/coco.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <set>
+
+#include "../core/ai.h"
+#include "../core/driver.h"
+
+using namespace std;
+
+namespace {
+
+class CompareRiver {
+public:
+    bool operator()(const River& r1, const River& r2) const {
+        if (r1.source != r2.source) return r1.source < r2.source;
+        if (r1.target != r2.target) return r1.target < r2.target;
+        return false;
+    }
+};
+
+class Coco : public AI {
+    void Init(int id, int num_punters, const Map* map) override {
+        map_ = map;
+    }
+
+    void LoadState(Json&& json) override {
+        for (const Json& river_json : json) {
+            const SiteId source = river_json["source"];
+            const SiteId target = river_json["target"];
+            rivers_.insert({source, target});
+        }
+    }
+
+    Json SaveState() override {
+        Json json;
+        for (const River& river : rivers_) {
+            json.push_back({{"source", river.source}, {"target", river.target}});
+        }
+        return json;
+    }
+
+    void Setup() override {
+        rivers_.insert(map_->rivers().begin(), map_->rivers().end());
+
+        for (const SiteId mine : map_->mines()) {
+            cout << "mine:" << mine << endl;
+        }
+    }
+
+    Move Gameplay(const std::vector<Move>& moves) override {
+        for (const Move& m : moves) {
+            if (m.action == kClaim) rivers_.erase(m.river);
+        }
+        if (rivers_.empty()) {
+            return {kPass, {}};
+        } else {
+            return {kClaim, *rivers_.begin()};
+        }
+    }
+
+    const Map* map_;
+    set<River, CompareRiver> rivers_;
+};
+
+}  // namespace
+
+int main() {
+    Coco ai;
+    Run(&ai);
+    return 0;
+}
