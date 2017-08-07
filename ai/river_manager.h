d5f06df7d66c14eb7a769c5692302c686ffdb6fd
Yusuke Izumi <yuizumi@y5i.org>
2017-08-07 12:27:22 +0900

    Added RiverManager. (#67)

---
+++ ai/river_manager.h
@@ -0,0 +1,31 @@
+#ifndef RIVER_MANAGER_H_
+#define RIVER_MANAGER_H_
+
+#include <map>
+
+#include "../core/ai.h"
+#include "../core/json.h"
+#include "river_set.h"
+
+class RiverManager {
+public:
+    using RiverMap = std::map<River, Move::Action, CompareRiver>;
+
+    explicit RiverManager(const Map& map, bool option);
+    RiverManager() = default;  // For Json.
+
+    const RiverMap& map() const { return map_; }
+    void HandleClaim(const River& river);  // claim or option
+
+private:
+    friend void from_json(const Json& json, RiverManager& value);
+    friend void to_json(Json& json, const RiverManager& value);
+
+    RiverMap map_;
+    bool option_;
+};
+
+void from_json(const Json& json, RiverManager& value);
+void to_json(Json& json, const RiverManager& value);
+
+#endif  // RIVER_MANAGER_H_
